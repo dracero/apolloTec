e90ab3b348c78509b4716c0f910a2c85bf90e3de
unknown <diego.racero@gmail.com>
2021-04-21 20:13:26 -0300

    second

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express');
+/*const express = require('express');
 const cors = require('cors')
 const { ApolloServer, gql } = require('apollo-server-express');
 
@@ -20,7 +20,36 @@ const server = new ApolloServer({ typeDefs, resolvers });
 
 const app = express();
 app.use(cors())
-server.applyMiddleware({ app, path: '/specialUrl'});
+var url = '/specialUrl' 
+server.applyMiddleware({ app, path: url });
 
-app.listen(process.env.PORT || 3000, 
-	() => console.log("Server is running..."));
+app.listen(process.env.PORT || 4000, 
+	() => console.log(`🚀 Server ready at ${url}`));*/
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+async function startApolloServer() {
+  // Construct a schema, using GraphQL schema language
+  const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+  `;
+
+  // Provide resolver functions for your schema fields
+  const resolvers = {
+    Query: {
+      hello: () => 'Hello world!',
+    },
+  };
+
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+
+  const app = express();
+  server.applyMiddleware({ app });
+
+  await new Promise(resolve => app.listen({ port: process.env.PORT || 4000 }, resolve));
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`);
+  return { server, app };
+}
