b01c982727f3df21dc5b9c938c6d3ebab19b6231
dracero <diego.racero@gmail.com>
2021-04-21 20:33:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const resolvers = {
   },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers, introspection: true, playground: true });
+const server = new ApolloServer({ introspection: true, playground: true });
 
 const app = express();
 app.use(cors())
